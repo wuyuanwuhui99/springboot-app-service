7f8a614add83d284b006955bc83d05f7292f04e5
wuyuanwuhui99 <80186116+wuyuanwuhui99@users.noreply.github.com>
2021-03-18 14:44:58 +0800

    Update IndexController.java

--- player-music/src/main/java/com/player/playermusic/controller/IndexController.java
+++ player-music/src/main/java/com/player/playermusic/controller/IndexController.java
@@ -30,25 +30,4 @@ public class FileStaticController {
     public String index() {
         return "forward:index.html";
     }
-
-    @GetMapping("/static/**")
-    public ResponseEntity<byte[]> getFile(HttpServletRequest request) {
-        try {
-            String servletPath = request.getServletPath();
-            String filePath = root.replace("/","") + servletPath;
-            File file = new File(filePath);
-            ResponseEntity.BodyBuilder bodyBuilder = ResponseEntity.ok();
-            bodyBuilder.contentLength(file.length());
-            // 二进制数据流
-            bodyBuilder.contentType(MediaType.APPLICATION_OCTET_STREAM);
-            // 在浏览器中打开
-            URL url = new URL("file:///" + file);
-            bodyBuilder.header("Content-Type", url.openConnection().getContentType());
-            String fileName = filePath.substring(filePath.lastIndexOf('/') + 1, filePath.length());
-            bodyBuilder.header("Content-Disposition", "inline;filename*=UTF-8''" + fileName);
-            return bodyBuilder.body(FileUtils.readFileToByteArray(file));
-        } catch (Exception e) {
-            return null;
-        }
-    }
 }
