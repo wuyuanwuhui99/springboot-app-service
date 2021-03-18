7ce79dddab4efbbdd1c23751cda01268fa01d17d
wuyuanwuhui99 <80186116+wuyuanwuhui99@users.noreply.github.com>
2021-03-18 11:30:37 +0800

    Update JwtTokenConfiguration.java

--- player-movie/src/main/java/com/player/movie/config/JwtTokenConfiguration.java
+++ player-movie/src/main/java/com/player/movie/config/JwtTokenConfiguration.java
@@ -2,8 +2,20 @@
 package com.player.movie.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import com.player.common.utils.JwtToken;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class JwtTokenConfiguration {
+    @Value("${token.secret}")
+    private String secret;
 
+    @Value("${token.expiration-time}")
+    private Long expirationTime;
+  
+    @Bean
+    public JwtToken getJwtToken() {
+        return new JwtToken(secret,expirationTime);
+    }
 }
